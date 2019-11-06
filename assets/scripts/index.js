5db5240ad0577e649661644d9407e505326fd792
Mondal10 <amitmondal111.am@gmail.com>
2019-11-06 18:06:21 +0530

    Scan uploaded image feature added

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,5 +1,5 @@
 function startProcessing() {
-    let img = document.getElementById('ocr');
+    let img = document.getElementById('ocr-img');
     Tesseract.recognize(
         img,
         'eng',
@@ -8,12 +8,12 @@ function startProcessing() {
         return res;
       }).then(({data}) => {
         const resultDiv = document.getElementById('result');
-        resultDiv.innerText = `Result of Scanned Image: ${data.text}`;
+        resultDiv.innerHTML = `<span>Result of Scanned Image:</span><div contenteditable=true>${data.text}</div>`;
         // console.log(data.text, typeof(data.text)); // returns type as string
         // console.log(Number(data.text), parseInt(data.text)); // converting string to number
 
-        let convertedNumber = [...data.text].map((num) => Number(num));
-        let sum = convertedNumber.reduce((acc, curr) => acc + curr);
+        // let convertedNumber = [...data.text].map((num) => Number(num));
+        // let sum = convertedNumber.reduce((acc, curr) => acc + curr);
 
         // console.log(':::SUM:::',sum);
       })
@@ -22,8 +22,20 @@ function startProcessing() {
 function scanningProgress(m) {
   document.getElementById('progress').innerText = 'Scanning...'
   if (m.status === 'recognizing text') {
-    document.getElementById('progress').innerText = `Scanning... ${m.progress * 100}%`;
+    document.getElementById('progress').innerText = `Scanning... ${Math.round(m.progress * 100)}%`;
   }
 }
 
 // For image uploader visit https://stackoverflow.com/questions/12368910/html-display-image-after-selecting-filename
+function readURL(input) {
+  if (input.files && input.files[0]) {
+      const reader = new FileReader();
+
+      reader.onload = function (e) {
+        const uploadedImg = document.getElementById('ocr-img');
+        uploadedImg.src = e.target.result;
+      };
+
+      reader.readAsDataURL(input.files[0]);
+  }
+}
