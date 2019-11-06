1b4483cd9cc4650bdab20155d8b7448ca7f0bc75
Mondal10 <amitmondal111.am@gmail.com>
2019-11-06 16:51:29 +0530

    updated with scaning progress

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,20 +3,27 @@ function startProcessing() {
     Tesseract.recognize(
         img,
         'eng',
-        { logger: m => console.log(m) }
+        { logger: m => scanningProgress(m) }
       ).then((res) => {
         return res;
       }).then(({data}) => {
         const resultDiv = document.getElementById('result');
         resultDiv.innerText = `Result of Scanned Image: ${data.text}`;
-        console.log(data.text, typeof(data.text)); // returns type as string
-        console.log(Number(data.text), parseInt(data.text)); // converting string to number
+        // console.log(data.text, typeof(data.text)); // returns type as string
+        // console.log(Number(data.text), parseInt(data.text)); // converting string to number
 
         let convertedNumber = [...data.text].map((num) => Number(num));
         let sum = convertedNumber.reduce((acc, curr) => acc + curr);
 
-        console.log(':::SUM:::',sum);
+        // console.log(':::SUM:::',sum);
       })
+}
+
+function scanningProgress(m) {
+  document.getElementById('progress').innerText = 'Scanning...'
+  if (m.status === 'recognizing text') {
+    document.getElementById('progress').innerText = `Scanning... ${m.progress * 100}%`;
+  }
 }
 
 // For image uploader visit https://stackoverflow.com/questions/12368910/html-display-image-after-selecting-filename
