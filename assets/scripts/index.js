00c8f7181d1e4fccf4d00609f4716b512ef52c14
Mondal10 <amitmondal111.am@gmail.com>
2019-11-16 21:29:53 +0530

    cleaning and linting

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,22 +1,15 @@
 function startProcessing() {
-    let img = document.getElementById('ocr-img');
-    Tesseract.recognize(
-        img,
-        'eng',
-        { logger: m => scanningProgress(m) }
-      ).then((res) => {
-        return res;
-      }).then(({data}) => {
-        const resultDiv = document.getElementById('result');
-        resultDiv.innerHTML = `<span>Result of Scanned Image:</span><div contenteditable=true>${data.text}</div>`;
-        // console.log(data.text, typeof(data.text)); // returns type as string
-        // console.log(Number(data.text), parseInt(data.text)); // converting string to number
-
-        // let convertedNumber = [...data.text].map((num) => Number(num));
-        // let sum = convertedNumber.reduce((acc, curr) => acc + curr);
-
-        // console.log(':::SUM:::',sum);
-      })
+  let img = document.getElementById('ocr-img');
+  Tesseract.recognize(
+    img,
+    'eng',
+    { logger: m => scanningProgress(m) }
+  ).then((res) => {
+    return res;
+  }).then(({ data }) => {
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = `<span>Result of Scanned Image:</span><div contenteditable=true>${data.text}</div>`;
+  })
 }
 
 function scanningProgress(m) {
@@ -29,13 +22,13 @@ function scanningProgress(m) {
 // For image uploader visit https://stackoverflow.com/questions/12368910/html-display-image-after-selecting-filename
 function readURL(input) {
   if (input.files && input.files[0]) {
-      const reader = new FileReader();
+    const reader = new FileReader();
 
-      reader.onload = function (e) {
-        const uploadedImg = document.getElementById('ocr-img');
-        uploadedImg.src = e.target.result;
-      };
+    reader.onload = function (e) {
+      const uploadedImg = document.getElementById('ocr-img');
+      uploadedImg.src = e.target.result;
+    };
 
-      reader.readAsDataURL(input.files[0]);
+    reader.readAsDataURL(input.files[0]);
   }
 }
